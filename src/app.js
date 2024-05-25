feee31c390e22bdbf0db852d85bf3ff9fdb780e0
Isha12D <ishadoifode6@gmail.com>
2024-05-26 00:08:13 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser"//cookie - to read users cookie
 
 const app = express()
 app.use(cors({
-    origin: provess.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
@@ -15,5 +15,12 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes import
+import userRouter from './routes/user.routes.js';
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
 
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -20,7 +20,7 @@
 
 //PROMISE METHOD
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).
         catch((err) => next(err))
     }
