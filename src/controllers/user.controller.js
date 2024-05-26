6693454b7673b5a776a9d4b4a459da44b35faf19
Isha12D <ishadoifode6@gmail.com>
2024-05-26 14:47:43 +0530

    5 steps of user reg done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,52 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js";
+import {User} from "../models/user.models.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
 
-const registerUser = asyncHandler(async(rq,res) => {
-     res.status(200).json({
-        message:"ok"
+const registerUser = asyncHandler(async(req,res) => {
+    //S T E P S
+    //get user details from frontend(we can also take from postman)
+    //validation
+    //check if user already exists(from username aND email)
+    //check for images, check for avatar
+    //upload them to cloudinary, avatar
+    //create user object - create entry in db
+    //remove password and refresh token field from response
+    //CHECK FOR USER CREATION
+    //RETURN RES
+
+    //step1
+    const {fullname, username, email, password } = req.body;
+    console.log("email:", email);
+
+    //step 2
+    if(
+        [fullname, email, username, password].some((field) => field?.trim() === "")
+    ){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    //step 3
+    const existedUser = User.findOne({
+        $or:[{username},{email}]
     })
+    if(existedUser){
+        throw new ApiError(409, "User with email or username already exists.")
+    }
+
+    //step 4
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is required")
+    }
+
+    //step 5
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 })
 
-export {registerUser}
+export {
+    registerUser,
+}
