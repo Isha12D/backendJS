851922dc2a10694ffae94fe13f982d75f0cea395
Isha12D <ishadoifode6@gmail.com>
2024-05-29 15:24:05 +0530

    update cuurentUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -180,9 +180,6 @@ return res
 .json(new ApiResponse(200, {}, "User logged Out"))
 })
 
-export {
-    registerUser, loginUser, logoutUser
-}
 
 const refreshAccesssToken = asyncHandler(async(req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
@@ -256,7 +253,7 @@ const changeCurrentUserPassword = asyncHandler(async(req, res) => {
 const getCurrentUser = asyncHandler(async(req, res) => {
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async(req, res) => {
@@ -267,7 +264,7 @@ const updateAccountDetails = asyncHandler(async(req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
        {
          $set:{
